a790193a63455bd56267c9b5db4a1771e5a30e6c
Arnav Garg <arnavga@gmail.com>
2018-02-01 20:09:33 -0800

    wrote charutils

---
+++ include/utils/charutil.hpp
@@ -0,0 +1,11 @@
+class CharUtil {
+public:
+    static bool isAlhpa(char c);
+    static bool isDigit(char c);
+    static bool isWhiteSpace(char c);
+    static bool isDelimiter(char c);
+    static bool isOperator(char c);
+    static bool isBeginIdentifier(char c);
+    static bool isPartIdentifier(char c);
+    static bool isBeginNumber(char c);
+};
---
+++ src/utils/charutil.cpp
@@ -0,0 +1,33 @@
+#include "utils/charutil.hpp"
+
+bool CharUtil::isAlhpa(char c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+bool CharUtil::isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+bool CharUtil::isWhiteSpace(char c) {
+    return "\t\r\f\v\u00A0\u2028\u2029".contains(c);
+}
+
+bool CharUtil::isDelimiter(char c) {
+    return c == ':' || c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']' || c == ';' || c == '\n';
+}
+
+bool CharUtil::isOperator(char c) {
+    return c == '+' || c == '-' || c == '/' || c == '*' || c == '%' || c == '<' || c == '>' || c == '=';
+}
+
+bool CharUtil::isBeginIdentifier(char c) {
+    return isAlhpa(c) || c == '_';
+}
+
+bool CharUtil::isPartIdentifier(char c) {
+    return isAlhpa(c) || isDigit(c) || c == '_';
+}
+
+bool CharUtil::isBeginNumber(char c) {
+    return isDigit(c) || c == '.';
+}
