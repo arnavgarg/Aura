e95eadd0955781cc207f64d6c0f94101340db26c
Arnav Garg <arnavga@gmail.com>
2018-02-02 23:01:22 -0800

    extended functionality

--- include/util/charutil.hpp
+++ include/util/charutil.hpp
@@ -1,4 +1,4 @@
-class CharUtil {
+class CharUtils {
 public:
     static bool isAlhpa(char c);
     static bool isDigit(char c);
@@ -8,4 +8,7 @@ public:
     static bool isBeginIdentifier(char c);
     static bool isPartIdentifier(char c);
     static bool isBeginNumber(char c);
+    static bool isBeginString(char c);
+    static bool isEndline(char c);
+    static bool isSpace(char c);
 };
--- src/util/charutil.cpp
+++ src/util/charutil.cpp
@@ -2,35 +2,46 @@
 
 #include "util/charutil.hpp"
 
-bool CharUtil::isAlhpa(char c) {
+bool CharUtils::isAlhpa(char c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 
-bool CharUtil::isDigit(char c) {
+bool CharUtils::isDigit(char c) {
     return c >= '0' && c <= '9';
 }
 
-bool CharUtil::isWhiteSpace(char c) {
+bool CharUtils::isWhiteSpace(char c) {
     std::string str("\t\r\f\v\u00A0\u2028\u2029");
     return str.find(c) != std::string::npos;
 }
 
-bool CharUtil::isDelimiter(char c) {
+bool CharUtils::isDelimiter(char c) {
     return c == ':' || c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']' || c == ';' || c == '\n';
 }
 
-bool CharUtil::isOperator(char c) {
+bool CharUtils::isOperator(char c) {
     return c == '+' || c == '-' || c == '/' || c == '*' || c == '%' || c == '<' || c == '>' || c == '=';
 }
 
-bool CharUtil::isBeginIdentifier(char c) {
+bool CharUtils::isBeginIdentifier(char c) {
     return isAlhpa(c) || c == '_';
 }
 
-bool CharUtil::isPartIdentifier(char c) {
+bool CharUtils::isPartIdentifier(char c) {
     return isAlhpa(c) || isDigit(c) || c == '_';
 }
 
-bool CharUtil::isBeginNumber(char c) {
+bool CharUtils::isBeginNumber(char c) {
     return isDigit(c) || c == '.';
+}
+
+bool CharUtils::isBeginString(char c) {
+    return c == '"';
+}
+
+bool CharUtils::isEndline(char c) {
+    return c == '\n';
+}
+bool CharUtils::isSpace(char c) {
+    return c == ' ';
 }
