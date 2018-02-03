030c2950010c3403df71e2e7467b418de548dded
Arnav Garg <arnavga@gmail.com>
2018-02-02 23:00:47 -0800

    added string reference array

--- include/lexer/tokentype.hpp
+++ include/lexer/tokentype.hpp
@@ -43,11 +43,62 @@ enum TokenType {
     LeftBlock,
     RightBlock,
     Semicolon,
-    Newline,
+    Endline,
 
     // misc
+    Space,
     EndOfInput,
     Unrecognized
+};
+
+const char* const typeStrings[] = {
+    // keywords
+    "If",
+    "Else",
+    "For",
+    "While,"
+    "Null",
+    "Func",
+    "Return",
+
+    // arithmatic operators
+    "Add",
+    "Sub",
+    "Div",
+    "Mul",
+    "Mod",
+
+    // comparision operators
+    "Less",
+    "LessEqual",
+    "Greater",
+    "GreatEqual",
+    "Equal",
+
+    // assignment operator
+    "Assign",
+
+    // types
+    "Int",
+    "Double",
+    "Bool",
+    "String",
+
+    // Delimiters
+    "Colon",
+    "LeftParen",
+    "RightParen",
+    "LeftBrace",
+    "RightBrace",
+    "LeftBlock",
+    "RightBlock",
+    "Semicolon",
+    "Endline",
+
+    // misc
+    "Space",
+    "EndOfInput",
+    "Unrecognized"
 };
 
 #endif
